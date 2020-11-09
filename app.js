fc3b05ba592c6230468ca5d9bde68a584892872a
Ilya Goss <goss@corp.ifmo.ru>
2020-11-09 06:53:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-module.exports = function(express, fs) {
+module.exports = function(express, bodyParser, fs, http) {
     const app = express();
 
     app
@@ -7,7 +7,7 @@ module.exports = function(express, fs) {
     .all('*', (req, res) => res.send('Working!'));
 
     // проверяющий алгоритм может запустить этот код в своей песочнице
-    // передав ему express и fs
+    // передав ему перечисленные зависимости
     // и сравнить то, что выдаёт маршрут /login/ работающего приложения
     // по предоставляемому обучающимся адресу
     // с тем, что выдаёт по этому же маршруту вот этот код в песочнице
