33be828f93569425d16337d3f88833a321aa29b9
Ilya Goss <goss@corp.ifmo.ru>
2020-11-09 07:27:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-module.exports = function(express, bodyParser, fs, http) {
+export default (express, bodyParser, fs, crypto, http) => {
     const app = express();
 
     app
     .get('/login/', (req, res) => res.send('eliasgoss'))
-    .get('/code/', (req, res) => {});
-    
-    //... остальные маршруты
-
+    .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res));
+   
+    // другие маршруты
 
     // проверяющий алгоритм может запустить этот код в своей песочнице
-    // передав ему перечисленные зависимости
+    // передав ему express и fs
     // и сравнить то, что выдаёт маршрут /login/ работающего приложения
     // по предоставляемому обучающимся адресу
     // с тем, что выдаёт по этому же маршруту вот этот код в песочнице
