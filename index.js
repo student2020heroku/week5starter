e5a24de2b12d0c476fecad7f897713d161da0664
student2020heroku <72618352+student2020heroku@users.noreply.github.com>
2020-11-21 20:57:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import fs from 'fs';
+import { createReadStream } from 'fs';
 import crypto from 'crypto';
 import http from 'http';
 
 import appSrc from './app.js';
 
-const app = appSrc(express, bodyParser, fs, crypto, http);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http);
 
 app.listen(process.env.PORT ?? 4321);
